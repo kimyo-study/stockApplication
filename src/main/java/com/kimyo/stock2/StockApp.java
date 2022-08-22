2ab38354b26d7e3e1679b8be732434dc4513b729
kimziou77 <kimziou77@naver.com>
2022-08-22 15:55:44 +0900

    chore: redis 추가

--- src/main/java/com/kimyo/stock2/StockApp.java
+++ src/main/java/com/kimyo/stock2/StockApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Stock2Application {
+public class StockApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Stock2Application.class, args);
+        SpringApplication.run(StockApp.class, args);
     }
 
 }
