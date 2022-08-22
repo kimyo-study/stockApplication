d4271cbe7bf607bce8a7da165812876a968ff1aa
kimziou77 <kimziou77@naver.com>
2022-08-22 13:01:05 +0900

    initial commit

---
+++ src/main/java/com/kimyo/stock2/Stock2Application.java
@@ -0,0 +1,13 @@
+package com.kimyo.stock2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stock2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stock2Application.class, args);
+    }
+
+}
